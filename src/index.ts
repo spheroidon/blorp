c662680d765a50167a479103fb1f0433f76dac47
Spheroidon <spheroidon@gmail.com>
2025-07-04 17:03:24 +0300

    Removed unnecessary comment

--- src/index.ts
+++ src/index.ts
@@ -11,24 +11,6 @@ const client = new Client({
 
 client.cooldowns = new Collection();
 
-// const foldersPath = path.join(__dirname, 'commands');
-// const commandFolders = fs.readdirSync(foldersPath);
-
-// for (const folder of commandFolders) {
-//     const commandsPath = path.join(foldersPath, folder);
-//     const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts') || file.endsWith('.js'));
-//     for (const file of commandFiles) {
-//         const filePath = path.join(commandsPath, file);
-//         const command = require(filePath);
-
-//         if ('data' in command && 'execute' in command) {
-//             client.commands.set(command.data.name, command);
-//         } else {
-//             console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
-//         }
-//     }
-// }
-
 loadCommands(client);
 loadEvents(client);
 
