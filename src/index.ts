ead8b0d81a881e29e3168db46ebfc1bc59d002df
Spheroidon <spheroidon@gmail.com>
2025-07-04 11:43:12 +0300

    Blorp is online! :D

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,9 @@ import { Client } from 'discord.js';
 const client = new Client({
     intents: ['Guilds', 'GuildMessages', 'GuildMembers', 'MessageContent']
 })
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}`);
+});
+
+client.login(process.env.BOT_TOKEN)
