4de58ad3dbd67cb01594741371e3df284a8f4088
Spheroidon <spheroidon@gmail.com>
2025-07-04 15:41:30 +0300

    Added 8ball command

---
+++ src/commands/fun/8ball.ts
@@ -0,0 +1,36 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction, EmbedBuilder } from 'discord.js';
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('8ball')
+        .setDescription('Ask the magic 8-ball a question!')
+        .addStringOption(option =>
+            option.setName('question')
+                .setDescription('The question you want to ask the magic 8-ball')
+                .setRequired(true)),
+    async execute(interaction: ChatInputCommandInteraction ) {
+        const responses = [
+            'Yes', 'No', 'Maybe', 'Ask again later', 'Definitely', 'Absolutely not',
+            'It is certain', 'Very doubtful', 'Without a doubt', 'My sources say no',
+            'As I see it, yes', 'Outlook not so good', 'Yes, in due time', 'You may rely on it',
+            'Concentrate and ask again', 'Better not tell you now', 'Cannot predict now', 'Don’t count on it',
+            'Most likely', 'Outlook good', 'Yes, definitely', 'You can count on it', 'It is decidedly so',
+            'My reply is no', 'Yes, in the near future', 'The outlook is not so good', 'Yes, but be cautious',
+            'In this economy?!', 'Idk mate', 'No idea buddy', 'Ask your mom', 'The stars say no',
+            'You can certainly try', 'Hell no', 'Heck yeah'
+        ];
+        const result = responses[Math.floor(Math.random() * responses.length)];
+
+        // Set a random color for the embed
+        const colors = [0xFF5733, 0x33FF57, 0x3357FF, 0xF1C40F, 0x8E44AD];
+        const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+        const embed = new EmbedBuilder()
+            .setColor(randomColor)
+            .setTitle('Magic 8-Ball Response')
+            .setDescription(`**Question:** ${interaction.options.getString('question')}\n**Answer:** ${result}`)
+            .setTimestamp();
+
+        await interaction.reply({embeds: [embed]});
+    }
+}
